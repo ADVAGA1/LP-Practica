61cddbad2d2acfadc40fcf6d9cf70e3092344704
ADVAGA1 <64835607+ADVAGA1@users.noreply.github.com>
2022-12-19 15:55:12 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,28 @@
-from flask import Flask
-from markupsafe import escape
+from flask import Flask, render_template, request
+from antlr4 import *
+from ExprLexer import ExprLexer
+from ExprParser import ExprParser
+from EvalVisitor import EvalVisitor
 
 app = Flask(__name__)
+visitor = EvalVisitor()
 
 @app.route("/")
-def index():
-    return "Index Page"
+def hello_world():
+    return render_template("index.html")
 
-@app.route("/hello")
-def hello():
-    return "Hello, World"
+@app.route("/result",methods = ["POST","GET"])
+def result():
+    if request.method == "POST":
+        result = request.form
+        antlr = InputStream(result["input"])
+        lexer = ExprLexer(antlr)
+        token_stream = CommonTokenStream(lexer)
+        parser = ExprParser(token_stream)
+        tree = parser.root()
+        try:
+            res = visitor.visit(tree)
+        except AssertionError as ex:
+            print(ex)
+        return render_template("result.html",result = res)
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)}'
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
-
-#if __name__ == "__main__":
- #   app.run(debug=True)
